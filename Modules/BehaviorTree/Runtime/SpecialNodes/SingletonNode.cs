da58403ea842fd17e352c73aa8ac3fc5ce766e3a
schenGameDev <166966274+schenGameDev@users.noreply.github.com>
2026-04-29 09:26:26 +0800

    Fix namespace reference for GUID in SingletonNode; update using directive to use UnityEngine.GUID

--- Modules/BehaviorTree/Runtime/SpecialNodes/SingletonNode.cs
+++ Modules/BehaviorTree/Runtime/SpecialNodes/SingletonNode.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using GUID = UnityEditor.GUID;
+using GUID = UnityEngine.GUID;
 
 namespace ModularFramework.Modules.BehaviorTree
 {
